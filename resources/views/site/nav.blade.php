bd753f9a28ef2c1568e7a412cb7c9edc58b23da0
RJS <premonday@gmail.com>
2019-05-02 16:29:27 +0200

    Built for prod, project restructured and optimised, lots of new components from the global design

--- resources/views/site/nav.blade.php
+++ resources/views/site/nav.blade.php
@@ -1,8 +1,20 @@
-@component('g.layouts.nav')
+@component('g.layouts.header')
+    @slot('headerLeft') Left component here @endslot
+    @slot('headerRight') Right component here @endslot
+    @slot('headerButtons')
+        <a href="#" class="d-none d-sm-inline-block btn btn-outline-white sm">Register</a>
+        <a href="#" class="btn btn-outline sm">Login</a>
+        @endslot
     @slot('sitename')
-        SiteName
+        Design System
     @endslot
+
     @slot('navLinks')
-        <li><a href="#">Link</a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#" class="active">Active Page</a></li>
+    @endslot
+
+    @slot('navExtra')
+        <li class="d-none d-sm-inline-block"><a href="#" class="active">Active link</a></li>
     @endslot
 @endcomponent
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,16 +7,41 @@
     @endphp
 @endsection
 
-@section('content')
-
+@section('hero')
     @component('g.partials.hero.home-1')
         @slot('class') hero-size-2 @endslot
-        @slot('title') Front-end design system @endslot
+        @slot('hero') https://mk11.tierlist.gg/img/hero.jpg @endslot
+        @slot('bgpos') 50% 20% @endslot
+        @slot('title') Mortal Kombat 11 Best Characters Tier List @endslot
+        @slot('subtitle') Cheat sheet for the top tier characters on MK11 @endslot
     @endcomponent
+@endsection
 
+@section('content')
     @component('g.layouts.content-sidebar')
-        @slot('content') Content Here @endslot
-        @slot('sidebar') Sidebar Content @endslot
+        @slot('content')
+            <h2>Global template header</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam, enim esse est ex expedita facere fugiat harum itaque iure laudantium quibusdam quisquam quos saepe sequi similique soluta velit veritatis.</p>
+            <hr class="my-4">
+            <div class="row">
+                <div class="col-sm-6">
+                    <h4>Header</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, cumque dicta dolor excepturi exercitationem, explicabo fuga incidunt nesciunt perspiciatis placeat quas similique suscipit, veniam. Ab eligendi et laboriosam quo! Harum!</p>
+                </div>
+                <div class="col-sm-6">
+                    <h4>Header</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, cumque dicta dolor excepturi exercitationem, explicabo fuga incidunt nesciunt perspiciatis placeat quas similique suscipit, veniam. Ab eligendi et laboriosam quo! Harum!</p>
+                </div>
+            </div>
+        @endslot
+        @slot('sidebar')
+            <div class="m-4">
+                <h3>Share on social</h3>
+                @include('g.components.social-share')
+            </div>
+
+            @include('g.components.login-register-sidebar')
+        @endslot
     @endcomponent
 
 @endsection
