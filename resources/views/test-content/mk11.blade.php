6a4bf607621e711ec44b4b8f92129319b70263ac
RJS <premonday@gmail.com>
2019-05-03 16:12:38 +0200

    /

--- resources/views/test-content/mk11.blade.php
+++ resources/views/test-content/mk11.blade.php
@@ -1,31 +1,40 @@
-<div class="row m-0">
-    <div class="col-lg-6">
-        <div class="bg-dark3 content-block h-100 border">
+<div class="row">
+    @component('g.partials.content.block')
+        @slot('block') col-lg-6 mb-3 @endslot
+        @slot('class') bg-dark3 @endslot
+        @slot('content')
             <small>Test Your Might</small>
             <h2>Mortal Kombat 11 Best Characters Tier List</h2>
             <p>Mortal Kombat 11 has just been released! Here you can see a tier list of the best characters. The S
                 Tier shows the best characters, incrementally getting worse as the tiers decrease. Our MK 11 Tier
                 List will be updated as the meta changes. Additional DLC characters will be added to the tier list
                 as they become available.</p>
-        </div>
-    </div>
-    <div class="col-lg-6">
-        <div class="bg-dark3 content-block h-100 border">
-            <small>The Community Tier List</small>
-            <h2>Contribute to the Mortal Kombat 11 Tier List</h2>
-            <p>As it's a new game with new characters, play styles and tricks are developed and can help determine a
-                character's performance in the game. If you feel like you know where a character belongs in the Tier
-                List, please get in touch with us at <a href="mailto:support@tierlist.gg">support@tierlist.gg</a>.
-            </p>
-        </div>
-    </div>
+        @endslot
+    @endcomponent
+        @component('g.partials.content.block')
+            @slot('block') col-lg-6 mb-3 @endslot
+            @slot('class') bg-dark3 @endslot
+            @slot('content')
+                    <small>The Community Tier List</small>
+                    <h2>Contribute to the Mortal Kombat 11 Tier List</h2>
+                    <p>As it's a new game with new characters, play styles and tricks are developed and can help determine a
+                        character's performance in the game. If you feel like you know where a character belongs in the Tier
+                        List, please get in touch with us at <a href="mailto:support@tierlist.gg">support@tierlist.gg</a>.</p>
+            @endslot
+        @endcomponent
 </div>
 
-<div class="p-4 mb-3 bg-black-30">
-    <div class="text-primary">A note about the Tier List</div>
-    <p class="mb-0 size-2">This tier list will be fluctuating a lot over the next weeks while we learn who
-        is stronger and weaker.</p>
-    <p class="weight-2 size-2 mb-0">Once we have collected enough votes, we will alter the tier list for MK11</p>
+<div class="row">
+    @component('g.partials.content.block')
+        @slot('block') mb-3 @endslot
+        @slot('class') bg-black @endslot
+        @slot('content')
+                <div class="text-primary">A note about the Tier List</div>
+                <p class="mb-0 size-2">This tier list will be fluctuating a lot over the next weeks while we learn who
+                    is stronger and weaker.</p>
+                <p class="weight-2 size-2 mb-0">Once we have collected enough votes, we will alter the tier list for MK11</p>
+        @endslot
+    @endcomponent
 </div>
 
 @component('g.components.list-module')
@@ -42,12 +51,21 @@
     @endslot
 @endcomponent
 
-<a href="/dlc-characters" class="btn btn-outline my-3">DLC Characters</a>
+<div class="row">
+    @component('g.partials.content.block')
+        @slot('block') mb-3 @endslot
+        @slot('class')  @endslot
+        @slot('content')
 
-<h2>Mortal Kombat 11 Tier List</h2>
-<p>This Mortal Kombat 11 Best Characters Tier List has been constructed with the help of beta testers, pro players and careful speculation. This Character Tier List will be updated frequently over the coming months as the game progresses and updates. Our Best Characters Tier List scales from S Tier to D Tier. D Tier shows the characters that are considered to be a bit underpowered in the current meta while S Tier shows the best characters in Mortal Kombat 11.</p>
+                <a href="/dlc-characters" class="btn btn-outline my-3">DLC Characters</a>
 
-<div class="tier-list d-flex flex-wrap">
-    <h2 style="flex:0 0 100%;" class="mb-3">Who are the Best Characters in Mortal Kombat 11?</h2>
+                <h2>Mortal Kombat 11 Tier List</h2>
+                <p>This Mortal Kombat 11 Best Characters Tier List has been constructed with the help of beta testers, pro players and careful speculation. This Character Tier List will be updated frequently over the coming months as the game progresses and updates. Our Best Characters Tier List scales from S Tier to D Tier. D Tier shows the characters that are considered to be a bit underpowered in the current meta while S Tier shows the best characters in Mortal Kombat 11.</p>
 
+                <div class="tier-list d-flex flex-wrap">
+                    <h2 style="flex:0 0 100%;" class="mb-3">Who are the Best Characters in Mortal Kombat 11?</h2>
+
+                </div>
+        @endslot
+    @endcomponent
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
 
 @section('hero')
     @component('g.partials.hero.home-1')
-        @slot('class') hero-size-3 shapes @endslot
+        @slot('class') hero-size-3 shapes overlap @endslot
         @slot('hero') https://mk11.tierlist.gg/img/hero.jpg @endslot
         @slot('bgpos') 50% 20% @endslot
         @slot('title') Mortal Kombat 11 Best Characters Tier List @endslot
@@ -18,6 +18,45 @@
 @endsection
 
 @section('content')
+
+    <div class="container">
+        <div class="row mb-5">
+            @component('g.partials.content.block')
+                @slot('image') https://placehold.it/200x300 @endslot
+                @slot('block') col-lg-4 mb-3 @endslot
+                @slot('class') bg-dark3 @endslot
+                @slot('content')
+                    <h2>Test Block</h2>
+                    <p>Mortal Kombat 11 has just been released! Here you can see a tier list of the best characters. The S
+                        Tier shows the best characters...</p>
+                        <a href="#" class="btn btn-outline">Button Outline</a>
+                @endslot
+            @endcomponent
+            @component('g.partials.content.block')
+                @slot('image') https://placehold.it/200x300 @endslot
+                @slot('block') col-lg-4 mb-3 @endslot
+                @slot('class') bg-dark3 @endslot
+                @slot('content')
+                    <h2>Test Block</h2>
+                    <p>Mortal Kombat 11 has just been released! Here you can see a tier list of the best characters. The S
+                        Tier shows the best characters...</p>
+                        <a href="#" class="btn btn-primary">Button Primary</a>
+                @endslot
+            @endcomponent
+            @component('g.partials.content.block')
+                @slot('image') https://placehold.it/200x300 @endslot
+                @slot('block') col-lg-4 mb-3 @endslot
+                @slot('class') bg-dark3 @endslot
+                @slot('content')
+                    <h2>Test Block</h2>
+                    <p>Mortal Kombat 11 has just been released! Here you can see a tier list of the best characters. The S
+                        Tier shows the best characters...</p>
+                        <a href="#" class="btn btn-secondary">Button Secondary</a>
+                @endslot
+            @endcomponent
+        </div>
+    </div>
+
     @component('g.layouts.content-sidebar')
         @slot('content')
             @include('test-content.mk11')
@@ -26,10 +65,14 @@
 
             @include('g.components.login-register-sidebar')
             @include('g.ads.300x250-unit')
-            <div class="m-4">
-                <h3>Share on social</h3>
-                @include('g.components.social-share')
-            </div>
+            @component('g.partials.content.block')
+                @slot('block') p-0 mt-3 @endslot
+                @slot('class') bg-dark3 @endslot
+                @slot('content')
+                    <h3>Share on social</h3>
+                    @include('g.components.social-share')
+                @endslot
+            @endcomponent
         @endslot
     @endcomponent
 
